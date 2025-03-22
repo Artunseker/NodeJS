30bc565d7f8d04cdf131b158a75140dc51b4d0db
ArtunSeker <artunseker2005@gmail.com>
2025-03-23 01:07:21 +0300

    Requeste, Response

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var http = require("http"); // import eder
 
-function requestListener(request,response){
-    response.end();//talebi döndürdüm 
-}
 
-var server = http.createServer(requestListener);
+var server = http.createServer((request,response)=>{
+    //console.log(request.url,request.method);
+    //console.log(response.statusCode);
+    response.setHeader('Content-Type',"text/html");
+    response.statusCode = 200;
+    response.statusMessage="OK";
+    
+    response.write("<h1> Ana Sayfa </h1>");
+    response.write("<p> Urunler </p>");
+
+    response.end();
+});
 
 server.listen(8000);//8000 den gelen istekleri kabul etcek
 
