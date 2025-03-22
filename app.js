17a2218faaf9132d584c6f19861456989212d15d
ArtunSeker <artunseker2005@gmail.com>
2025-03-23 00:47:52 +0300

    Http Server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("merhaba node");
+var http = require("http"); // import eder
+
+function requestListener(request,response){
+    response.end();//talebi döndürdüm 
+}
+
+var server = http.createServer(requestListener);
+
+server.listen(8000);//8000 den gelen istekleri kabul etcek
+
+console.log("Server is listening on port 8000");
