c8bc9a4632cff711509f6167d28ce007c999b3eb
ArtunSeker <artunseker2005@gmail.com>
2025-03-23 14:25:16 +0300

    File System

--- app.js
+++ app.js
@@ -1,53 +1,29 @@
 var http = require("http"); // import eder
-
+var fs = require("fs");
 
 var server = http.createServer((request,response)=>{
     if(request.url === "/"){
-        response.writeHead(200,{"Content-Type":"text/html"});
-        response.write(`
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Ana Sayfa</title>
-                </head>
-                <body>
-                    <h1>Hoş Geldiniz</h1>
-                </body>
-            
-            </html>`);
-        response.end();
+        fs.readFile("index.html",(error,html)=>{
+            response.writeHead(200,{"Content-Type":"text/html"});
+            response.write(html);
+            response.end();
+        });
     }
     else if(request.url === "/hakkimda"){
-        response.writeHead(200,{"Content-Type":"text/html"});
-        response.write(`
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Hakkımda</title>
-                </head>
-                <body>
-                    <h1>Hakkimda</h1>
-                </body>
-            
-            </html>`);
-        response.end();
+        fs.readFile("hakkımda.html",(error,html)=>{
+            response.writeHead(200,{"Content-Type":"text/html"});
+            response.write(html);
+            response.end();
+        });
     }
     else{
-        response.writeHead(404,{"Content-Type":"text/html"});
-        response.write(`
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>404</title>
-                </head>
-                <body>
-                    <h1>Sayfa Bulunamadi</h1>
-                </body>
-            
-            </html>`);
-        response.end();
+        fs.readFile("404.html",(error,html)=>{
+            response.writeHead(404,{"Content-Type":"text/html"});
+            response.write(html);
+            response.end();
+        });
     }
-    
+
 });
 
 server.listen(8000);//8000 den gelen istekleri kabul etcek
