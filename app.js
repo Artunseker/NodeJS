609e336b19f799d17828805830c8670d48457618
ArtunSeker <artunseker2005@gmail.com>
2025-03-23 13:13:15 +0300

    routing request

--- app.js
+++ app.js
@@ -2,16 +2,52 @@ var http = require("http"); // import eder
 
 
 var server = http.createServer((request,response)=>{
-    //console.log(request.url,request.method);
-    //console.log(response.statusCode);
-    response.setHeader('Content-Type',"text/html");
-    response.statusCode = 200;
-    response.statusMessage="OK";
+    if(request.url === "/"){
+        response.writeHead(200,{"Content-Type":"text/html"});
+        response.write(`
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Ana Sayfa</title>
+                </head>
+                <body>
+                    <h1>Hoş Geldiniz</h1>
+                </body>
+            
+            </html>`);
+        response.end();
+    }
+    else if(request.url === "/hakkimda"){
+        response.writeHead(200,{"Content-Type":"text/html"});
+        response.write(`
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Hakkımda</title>
+                </head>
+                <body>
+                    <h1>Hakkimda</h1>
+                </body>
+            
+            </html>`);
+        response.end();
+    }
+    else{
+        response.writeHead(404,{"Content-Type":"text/html"});
+        response.write(`
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>404</title>
+                </head>
+                <body>
+                    <h1>Sayfa Bulunamadi</h1>
+                </body>
+            
+            </html>`);
+        response.end();
+    }
     
-    response.write("<h1> Ana Sayfa </h1>");
-    response.write("<p> Urunler </p>");
-
-    response.end();
 });
 
 server.listen(8000);//8000 den gelen istekleri kabul etcek
