e38b242e95bc7c5d3e3977e9257e3d0325ac2f09
ArtunSeker <artunseker2005@gmail.com>
2025-03-23 20:47:38 +0300

    Post metod ve Form Data

--- app.js
+++ app.js
@@ -16,6 +16,39 @@ var server = http.createServer((request,response)=>{
             response.end();
         });
     }
+    else if(request.url === "/create"&&request.method === "POST"){
+        const data = [];
+        request.on("data",(chunk)=>{
+            data.push(chunk);
+        });
+
+        request.on("end",()=>{
+            const result = Buffer.concat(data).toString();
+            const parsedData = result.split("=")[1];
+            
+            fs.appendFile("data.txt",parsedData,(err) =>{//data.txt dosyasına parseddatayı yazdırır
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    response.statusCode = 302;
+                    response.setHeader("Location","/");//'/' sayfasına yönlendirme yapar
+                    response.end();
+                }
+            });
+
+        });
+
+        
+
+    }
+    else if(request.url === "/create"){
+        fs.readFile("create.html",(error,html)=>{
+            response.writeHead(200,{"Content-Type":"text/html"});
+            response.write(html);
+            response.end();
+        });
+    }
     else{
         fs.readFile("404.html",(error,html)=>{
             response.writeHead(404,{"Content-Type":"text/html"});
